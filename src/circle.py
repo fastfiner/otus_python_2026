0eeed21d4340de1590842c4f184231e6ef58ec4c
Ali Makhmadziyoev <alimakhmadziyoev@MacBook-Pro-Ali.local>
2026-06-12 01:10:58 +0300

    second hwork

---
+++ src/circle.py
@@ -0,0 +1,15 @@
+from src.figure import Figure
+
+class Circle(Figure):
+    def __init__(self, radius):
+        if radius <= 0:
+            raise ValueError("Circle radius can't be less than 0")
+        self.radius = radius
+
+    @property
+    def get_area(self):
+        return self.radius ** 2 * 3.14159
+
+    @property
+    def get_perimeter(self):
+        return self.radius * 2 * 3.14159
