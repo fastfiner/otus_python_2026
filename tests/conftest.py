1973d48dc1dbfc7b02cf1ca3936808634c7fcaf3
amakhmadzieev <fastfiner@gmail.com>
2026-07-07 21:22:09 +0300

    реализована 2 часть

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,22 @@
 import pytest
 
+
 def pytest_addoption(parser):
     parser.addoption(
-        "--env",           # имя параметра
-        action="store",    # что делать с переданным значением
-        default="dev",     # значение по умолчанию
-        help="Выберите окружение: dev, test, prod"  # подсказка
+        "--url",
+        default="https://ya.ru",
+        help="Укажите ссылку"
+    )
+    parser.addoption(
+        "--status_code",
+        default="200",
+        help="Выберите ожидаймый статус"
     )
 
 @pytest.fixture
-def env(request):
-    return request.config.getoption("--env")
+def url(request):
+    return request.config.getoption("--url")
+
+@pytest.fixture
+def status_code(request):
+    return request.config.getoption("--status_code")
